265a1e79a30f6a0e7cfa2bf94bc7f0f3497b19e3
Rob <robfraserdesign@gmail.com>
2020-09-20 12:01:11 +1000

    Fixed footer

--- footer.php
+++ footer.php
@@ -3,15 +3,16 @@
 	<?php } ?>
 
 	</div>
-	<!--#lc_swp_content-->
-	<?php get_sidebar('footer'); ?>
-	<?php get_template_part('views/canvas_image'); ?>
-	<?php get_template_part('views/utils/back_to_top'); ?>
-	<?php get_template_part('views/utils/search_form_global'); ?>
+		<!--#lc_swp_content-->
+		<?php get_sidebar('footer'); ?>
+		<?php get_template_part('views/canvas_image'); ?>
+		<?php get_template_part('views/utils/back_to_top'); ?>
+		<?php get_template_part('views/utils/search_form_global'); ?>
+		<?php get_template_part('views/powered_by'); ?>
 	</div>
 	<!--#lc_swp_wrapper-->
-	<?php wp_footer(); ?>
-	<?php get_template_part('views/powered_by'); ?>
+		<?php wp_footer(); ?>
+
 	</body>
 
 	</html>
--- views/powered_by.php
+++ views/powered_by.php
@@ -1,3 +1,3 @@
-<div style="text-align: center">
-    <p>Website powered by <a href="https://roo.digital" target="_blank">roo.digital</a></p>
+<div style="text-align: center; background: #1d1d1d; padding: 20px 0">
+    <h4 style="margin: 0 auto"><b>Website powered by <a href="https://roo.digital" target="_blank">roo.digital</a></b></h4>
 </div>
